4aaae0b0d444a2f726d4156a0d018d7d4e27d722
fmbearmf <77757734+fmbearmf@users.noreply.github.com>
2024-12-09 18:10:11 -0800

    changes

--- config.def.h
+++ config.def.h
@@ -26,9 +26,6 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -39,9 +36,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "<M>",      monocle },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "<M>",      monocle },
 };
 
 /* key definitions */
