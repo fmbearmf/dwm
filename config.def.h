6e6aa36e5500e4b4913f95387b8c8f6715327e9e
fmbearmf <77757734+fmbearmf@users.noreply.github.com>
2024-12-12 20:13:27 -0800

    ok

--- config.def.h
+++ config.def.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *j4cmd[] =    { "j4-dmenu-desktop", NULL };
-static const char *sscmd[] =    { "/bin/sh", "-c", "'scrot -s - | xclip -selection clipboard -t image/png'", NULL };
+static const char *sscmd[] =    { "~/.local/bin/screenshot", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 #include "selfrestart.c"
